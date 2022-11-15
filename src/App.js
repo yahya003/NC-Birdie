8392570c402e45d473d4c01422cd368ee4d6154e
Yahya <yahyaua5@gmail.com>
2022-11-15 15:36:10 +0000

    Sign Up page format complete

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Navigation from './Components/Navigation';
 import Home from './Components/Home';
 import SignIn from './Components/SignIn';
+import SignUp from './Components/SignUp';
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
         <Navigation/>
       <Routes>
         <Route path="/" element={<SignIn/>}></Route>
+        <Route path="/signup" element={<SignUp/>}></Route>
         <Route path="/home" element={<Home/>}></Route>
         <Route path="/capture"></Route>
         <Route path="/captured"></Route>
---
+++ src/Components/SignUp.jsx
@@ -0,0 +1,24 @@
+import { NavLink } from "react-router-dom"
+
+const SignUp = () => {
+    return (
+        <div className="signUpPage">
+          <h1>Sign Up</h1>
+          <form id="form" >
+            <label htmlFor="email">Username: </label>
+             <input id="email" required/>
+             <br/>
+    
+          <label htmlFor="password">Password: </label>
+            <input type="password"  id="password"  required /> 
+          </form>
+
+        <button className="signUp">Sign Up</button>
+        <br/>
+        <NavLink to ="/">Already have an account? Log In</NavLink>
+        </div>
+      )
+}
+
+
+export default SignUp
