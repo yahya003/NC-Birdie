7b200ccbce6439a164769c0f6b6e1622d388c466
Yahya <yahyaua5@gmail.com>
2022-11-15 15:53:12 +0000

    Captured

---
+++ src/Components/Captured.jsx
@@ -0,0 +1,9 @@
+const Captured = () => {
+  return (
+    <>
+        <h2 className="captureTitle">Birds you have captured</h2>
+    </>
+  )
+}
+
+export default Captured
--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,8 +1,9 @@
 const Home = () => {
     return (
-        
+        <>
+          <h1 className="captureTitle">Welcome Back</h1>
           <h2 className="captureTitle">Birds left to capture</h2>
-        
+        </>
     )
 }
 
