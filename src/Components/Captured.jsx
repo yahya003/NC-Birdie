d9477943734f1faae3a7c32644ab37292080128e
Yahya <yahyaua5@gmail.com>
2022-11-17 14:06:23 +0000

    Pagination added, needs some major styling!

--- src/Components/Captured.jsx
+++ src/Components/Captured.jsx
@@ -1,19 +1,27 @@
 import {birds} from "./bird_names.js"
 import CapturedCard from "./CapturedCard.jsx"
 import "../App.css";
+import { useState } from "react";
+import Pagination from "./Pagination.js";
 
 const Captured = () => {
+  const [currentPage, setCurrentPage] = useState(1)
+  const [postsPerPage, setPostsPerPage] = useState(30)
+
+  const indexOfLastPost = currentPage*postsPerPage
+  const indexOfFirstPost = indexOfLastPost - postsPerPage
+  const currentPost = birds.slice(indexOfFirstPost, indexOfLastPost)
 
   return (
     <>    
  
     <h3 className="captureTitle">Birds you have captured</h3>
+    <Pagination postsPerPage={postsPerPage} totalPosts={birds.length} setCurrentPage={setCurrentPage}/>
     <section className="all-birds">
-    {birds.map((bird) => {
+    
+    {currentPost.map((bird) => {
             return (
-              <CapturedCard
-                bird={bird}
-              />
+              <CapturedCard bird={bird}/>
             );
           })}
        
--- src/Components/CapturedCard.jsx
+++ src/Components/CapturedCard.jsx
@@ -1,13 +1,17 @@
 import silhoute from'../emptyBird.jpg';
 import "../App.css";
+import Pagination from './Pagination';
+
 
 const ShowCard = ({bird}) => {
 
     return (
-<captured className="captured-card">
+
+       <captured className="captured-card">
         <section className="card-info">
         <img src={silhoute} alt="bird silhoute" width="200" height="200"></img>
         <p className="captured-card-birdname">{bird}</p>
+
 
         </section>
 
