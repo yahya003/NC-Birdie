9c415959f05f3d08d0f31e39d1d14ae06c2fa0f4
harrys1000rr <harry@DESKTOP-NC80NV3.localdomain>
2022-11-22 10:25:02 +0000

    styling stugg

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -17,16 +17,26 @@ const Home = () => {
     const currentPost = birds.slice(indexOfFirstPost, indexOfLastPost)
     const [search, setSearch] = useState("")
     const {user} = useUserAuth()
+   
 
     const handleChange = (event) => {
+      
         setSearch(event.target.value)
     }
 
     return (
         <>
-            <h3 className="captureTitle"> Welcome back {user?.email} </h3>
-            <h4 className="captureTitle">Birds left to capture</h4>
-            <input onChange={handleChange} id = "search" className="search" placeholder="Search for a specific bird..."/>
+          <h3 className="WelcomeTitle"> {user?.email} </h3>
+          <div class="boxo"> 
+          <div class="box">
+          <form name="search">
+                  <input onChange={handleChange} class="input" placeholder="Search for a specific bird..."/>
+             
+                  </form>
+                  <i class="fas fa-search"></i></div>
+                  </div>
+             {/* <h4 className="captureTitle">Birds left to capture</h4> */}
+           
          
             
            <section className="all-birds">
